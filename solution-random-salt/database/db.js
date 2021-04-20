d3700d252522e6d57f6cfbf975456d7de838e43d
oliverjam <oliverjamesphillips@gmail.com>
2021-04-20 22:06:40 +0100

    Use native fs/promises APIs in DB

--- solution-random-salt/database/db.js
+++ solution-random-salt/database/db.js
@@ -1,4 +1,5 @@
-const fs = require("fs");
+const { existsSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs/promises");
 const path = require("path");
 
 const dbPath = path.join(__dirname, "db.json");
@@ -6,9 +7,9 @@ const initialData = { users: [] };
 
 // create db.json if it doesn't exist yet
 try {
-  const dbFileExists = fs.existsSync(dbPath);
+  const dbFileExists = existsSync(dbPath);
   if (!dbFileExists) {
-    fs.writeFileSync(dbPath, JSON.stringify(initialData, null, 2));
+    writeFileSync(dbPath, JSON.stringify(initialData, null, 2));
   }
 } catch (error) {
   console.error("Error creating db.json");
@@ -16,54 +17,27 @@ try {
 }
 
 function getUsers() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(dbPath, (error, contents) => {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(contents);
-        resolve(data.users);
-      }
-    });
-  });
+  return readFile(dbPath).then((contents) => JSON.parse(contents));
 }
 
 function getUser(email) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(dbPath, (error, contents) => {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(contents);
-        const user = data.users.find(user => user.email === email);
-        if (!user) {
-          reject(new Error(`${email} not found in users`));
-        } else {
-          resolve(user);
-        }
-      }
-    });
+  return readFile(dbPath).then((contents) => {
+    const data = JSON.parse(contents);
+    const user = data.users.find((user) => user.email === email);
+    if (!user) throw new Error(`${email} not found in users`);
+    return user;
   });
 }
 
 function createUser(user) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(dbPath, (error, contents) => {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(contents);
-        const existingUser = data.users.find(u => u.email === user.email);
-        if (existingUser) {
-          reject(new Error(`${user.email} already exists`));
-        } else {
-          user.id = Date.now(); // rudimentary unique ID
-          data.users.push(user);
-          fs.writeFile(dbPath, JSON.stringify(data, null, 2), error => {});
-          resolve(user);
-        }
-      }
-    });
+  return readFile(dbPath).then((contents) => {
+    const data = JSON.parse(contents);
+    const existingUser = data.users.find((u) => u.email === user.email);
+    if (existingUser) throw new Error(`${user.email} already exists`);
+
+    user.id = Date.now(); // rudimentary unique ID
+    data.users.push(user);
+    return writeFile(dbPath, JSON.stringify(data, null, 2)).then(() => user);
   });
 }
 
